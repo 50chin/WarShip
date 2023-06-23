db08f3f85fbab23f1a000f789ddea36ae9037ab4
Gigorii- <chepurnov2009@yandex.ru>
2023-06-23 15:23:00 +0300

    reli2

--- fire.js
+++ fire.js
@@ -1,8 +1,28 @@
+const generate = require("./generate");
+const readlineSync = require("readline-sync");
+const name = readlineSync.question("Напишите свое имя? ");
+console.log(`Игрок - ${name}`);
+const sound = require("play-sound")((opts = {}));
+sound.play("./sound/ortal_kombat_rival.wav")
+
 function fire() {
-  // здесь код, который меняет игровое поле после выстрела
+  const newArr = [...generate];
+  const arrMap = newArr.map((el) =>
+    el.map((elem) => {
+      let findEl = "💧";
+      if (elem === 1) {
+        sound.play("./sound/vyistrel-pistoleta-36125.wav");
+        findEl = "❌";
+      }
+
+      return findEl;
+    })
+  );
+  return arrMap.join("\n").replace(/,/g, "");
 }
 
 function showBattlefield() {
-  // здесь код, который показывает игровое поле в консоли
+  const fireRun = fire();
+  return fireRun;
 }
-
+console.table(showBattlefield());
